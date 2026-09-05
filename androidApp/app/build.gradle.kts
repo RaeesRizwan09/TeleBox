33bbf82548c6ef48db050b2ce6e18a3a374bb4ff
Rizwan Ali <96179641+RaeesRizwan09@users.noreply.github.com>
2026-09-05 10:36:16 +0500

    Updated build.gradle to acquire new UI features

--- androidApp/app/build.gradle.kts
+++ androidApp/app/build.gradle.kts
@@ -64,6 +64,7 @@ dependencies {
     implementation("androidx.compose.foundation:foundation")
     implementation("androidx.compose.animation:animation")
     implementation("androidx.activity:activity-compose:1.9.3")
+    implementation("androidx.core:core-ktx:1.15.0")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.8.7")
     implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.8.7")
     implementation("androidx.lifecycle:lifecycle-runtime-compose:2.8.7")
